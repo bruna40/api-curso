54048d54612d729325bc1b2e727c976b177a50c6
Bruna Santiago <brunasanthiago@outlook.com>
2025-01-14 12:00:36 -0300

    hash password

--- src/main/java/com/api/curso/api_curso/modules/users/useCases/UserUseCase.java
+++ src/main/java/com/api/curso/api_curso/modules/users/useCases/UserUseCase.java
@@ -1,6 +1,7 @@
 package com.api.curso.api_curso.modules.users.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.api.curso.api_curso.modules.users.entity.UserEntity;
@@ -11,9 +12,19 @@ public class UserUseCase {
     
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     
 
     public UserEntity execute(UserEntity userEntity) {
-        return userRepository.save(userEntity);
+        this.userRepository.findByEmail(userEntity.getEmail()).ifPresent(user -> {
+            throw new RuntimeException("Email já cadastrado");
+        });
+
+        var password = passwordEncoder.encode(userEntity.getPassword());
+        userEntity.setPassword(password);
+
+        return this.userRepository.save(userEntity);
     }
 }
